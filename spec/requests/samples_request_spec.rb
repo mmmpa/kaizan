a49d6cffa7d6ed51e13f03f538a4a137c9fc8e6f
mmmpa <mmmpa.mmmpa@gmail.com>
2015-06-27 15:28:17 +0900

    passed

--- spec/requests/samples_request_spec.rb
+++ spec/requests/samples_request_spec.rb
@@ -1,10 +1,39 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe 'Samples', type: :request do
-  describe do
-    it do
-      get '/samples/index'
-      response.status.should == 200
-    end
+  before :each do
+    get '/samples/index'
+  end
+
+  it 'not html_safe string, keep it' do
+    expect(response.body).not_to have_tag('li.case1 xmp')
+  end
+
+  it 'alter with not string' do
+    expect(response.body).to have_tag('li.case2', text: 'string')
+  end
+
+  it 'alter with not html_safe string' do
+    expect(response.body).not_to have_tag('li.case3 xmp')
+  end
+
+  it 'alter with html_safe string' do
+    expect(response.body).to have_tag('li.case4 strong')
+  end
+
+  it 'alter with named anchor' do
+    expect(response.body).to have_tag('li.case7', text: 'Thank you', count: 1)
+  end
+
+  it 'alter with same named anchor' do
+    expect(response.body).to have_tag('li', text: 'Multiple altering', count: 2)
+  end
+
+  it 'ignore anchor name not exist' do
+    expect(response.body).not_to include('Not exist')
+  end
+
+  it 'SafeBuffer can concatenate after altering' do
+    expect(response.body).to have_tag('footer', text: 'Concatenated', count: 1)
   end
 end
